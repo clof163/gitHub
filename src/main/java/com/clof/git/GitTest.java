c1c46276e2419baae229cd3ec43f07bdd06c87b6
clof <clof163@163.com>
2021-10-05 17:33:38 +0800

    three commit

--- src/main/java/com/clof/git/GitTest.java
+++ src/main/java/com/clof/git/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git.");
         System.out.println("hello two.");
+        System.out.println("hello three.");
     }
 
 }
