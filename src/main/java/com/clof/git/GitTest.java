c15a9bb39d5c298bbc16873b8b81874bb82e04af
clof <clof163@163.com>
2021-10-05 17:29:57 +0800

    first commit

---
+++ src/main/java/com/clof/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.clof.git;
+
+/**
+ * @author CLoF
+ * @create 2021-10-05 17:24
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git.");
+    }
+
+}
