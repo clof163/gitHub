a5d781ebed99a6bd47d4dbee3ba5e07246d8e15a
clof <clof163@163.com>
2021-10-05 17:32:26 +0800

    first commit

--- src/main/java/com/clof/git/GitTest.java
+++ src/main/java/com/clof/git/GitTest.java
@@ -7,6 +7,7 @@ package com.clof.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git.");
+        System.out.println("hello two.");
     }
 
 }
